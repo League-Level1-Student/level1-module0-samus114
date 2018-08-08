09db7a62d8ecc1473baf05122010347866dc1428
League Student <league@iMac-4.local>
2018-08-07 18:32:15 -0700

    houses, spinning record

---
+++ spinning_record/spinning_record.pde
@@ -0,0 +1,25 @@
+import ddf.minim.*;  //at the very top of your sketch
+PImage pictureOfRecord;  //as member variable
+int rotate = 0;
+Minim minim;  //as a member variable
+AudioPlayer song;  //as a member variable
+void setup(){
+size(800, 800);
+pictureOfRecord= loadImage("record.jpg");  //in setup method
+pictureOfRecord.resize(width,height);
+minim = new Minim(this);  //in the setup method
+song = minim.loadFile("Halo Theme Song Original.mp3", 512);//in the setup method
+song.play();
+}
+void draw(){
+if(mousePressed){
+  rotateImage(pictureOfRecord,rotate);
+image(pictureOfRecord,0,0);
+rotate += 10;
+}
+}
+void rotateImage(PImage image, int amountToRotate) {
+     translate(width/2, height/2);
+     rotate(amountToRotate*TWO_PI/360);
+     translate(-image.width/2, -image.height/2);
+}
--- src/houses.java
+++ src/houses.java
@@ -8,6 +8,7 @@ public class houses {
 	 static Random rand=new Random();
 	 
 	public static void main(String[] args) {
+	Robot.setWindowColor(0,0,0);
 	rob.setSpeed(500);
 	rob.moveTo(1, 550);
 	rob.turn(90);
@@ -15,27 +16,53 @@ public class houses {
 	for (int i = 0; i < 9; i++) {
 		int x = rand.nextInt(3);
 		//houseDrawer();
+	if (x==0) {
+		houseDrawer("small");
+	}else if (x==1) {
+		houseDrawer("medium");
+	} else {
+		houseDrawer("large");
+	}
 	}
 }
-static void houseDrawer(String size ) {
+static void houseDrawer(String size) {
 	int height = 0;
+	rob.setRandomPenColor();
+	rob.penDown();
+	boolean pointy = false;
 	if (size.equalsIgnoreCase("small")) {
 		height = 60;
+		pointy = true;
 	}else if (size.equalsIgnoreCase("medium")) {
 		height = 120;
+		pointy = true;
 	}else if (size.equalsIgnoreCase("large")) {
 		height = 250;
 	}
-	rob.setPenColor(0,0,0);
-	rob.turn(-90);
-	rob.penDown();
-	rob.move(height);
+		rob.turn(-90);
+		rob.move(height);
+		if (pointy) {
+			drawPointyRoof();
+		}else {
+			drawFlatRoof();
+		}
+		rob.move(height);
+		rob.turn(-90);
+		rob.setPenColor(0,255,0);
+		rob.move(60);
+
+
+}
+public static void drawPointyRoof() {
+	rob.turn(45);
+	rob.move(20);
+	rob.turn(90);
+	rob.move(20);
+	rob.turn(45);
+}
+public static void drawFlatRoof() {
 	rob.turn(90);
 	rob.move(30);
 	rob.turn(90);
-	rob.move(height);
-	rob.turn(-90);
-	rob.setPenColor(0,255,0);
-	rob.move(60);
 }
 }

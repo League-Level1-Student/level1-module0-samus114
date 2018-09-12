51ccbea4c92c4d840e8a7137f09f11316c9eccc2
GaynorGithub <league@wts-imac-11.attlocal.net>
2018-09-11 18:30:19 -0700

    effects machine ip

--- src/Effects_Machine.java
+++ src/Effects_Machine.java
@@ -6,27 +6,41 @@ import java.awt.event.ActionListener;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Effects_Machine implements ActionListener {
-	JFrame sam = new JFrame();
-	JButton Button = new JButton();
+	JPanel Panel = new JPanel();
+	JFrame Frame = new JFrame();
+	JButton Button = new JButton("Muscle car sound");
+	JButton Button2 = new JButton("marbles sound");
+	JButton Button3 = new JButton("Cartoon Phone");
 
 	public static void main(String[] args) {
 		new Effects_Machine().visible();
 	}
 
-	public void actionPerformed(ActionEvent event ) {
+	public void actionPerformed(ActionEvent event) {
 		event.getSource();
-		
-		if (event.getSource()==Button) {
+
+		if (event.getSource() == Button) {
 			playSound("muscle.wav");
+		}
+		else if (event.getSource() == Button2) {
+			playSound("marbles.wav");
+		}
+		else if (event.getSource() == Button3) {
+			playSound("CartoonPhone.wav");
 		}
 	}
 
 	public void visible() {
-		sam.setVisible(true);
-		sam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		Frame.add(Panel);
+		Frame.setVisible(true);
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Panel.add(Button);
+		Panel.add(Button2);
+		Panel.add(Button3);
+		Frame.pack();
 	}
 
 	private void playSound(String fileName) {

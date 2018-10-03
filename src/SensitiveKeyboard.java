97e11c7e31cd5756a85ebd1fcf5fb246f09a9b3f
Rohan890 <league@wts7.attlocal.net>
2018-10-02 18:27:05 -0700

    sensitive keyboard

--- src/SensitiveKeyboard.java
+++ src/SensitiveKeyboard.java
@@ -1,4 +1,37 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class SensitiveKeyboard {
-
+import javax.swing.JFrame;
+public class SensitiveKeyboard implements KeyListener{
+JFrame frame = new JFrame();
+String words = "ouch";
+	public static void main(String[] args) {
+	new SensitiveKeyboard().visible();
+}
+	public void visible() {
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+	}
+static void speak(String words) {
+    try {
+         Runtime.getRuntime().exec("say " + words).waitFor();
+    } catch (Exception e) {
+         e.printStackTrace();
+         }
+    }
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+System.out.println(words);
+	speak(words);
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub	
+}
 }
